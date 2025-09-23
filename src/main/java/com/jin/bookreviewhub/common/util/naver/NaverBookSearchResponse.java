5409182e44cc0da40f322d9a1b7ffe0e9711dad4
sungjin122517 <sungjin122517@naver.com>
2025-09-23 22:10:52 +0900

    add new DTO (NaverBookSearchResponse)

---
+++ src/main/java/com/jin/bookreviewhub/common/util/naver/NaverBookSearchResponse.java
@@ -0,0 +1,56 @@
+package com.jin.bookreviewhub.common.util.naver;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class NaverBookSearchResponse {
+
+    @JsonProperty("total")
+    private int total;
+
+    @JsonProperty("start")
+    private int start;
+
+    @JsonProperty("display")
+    private int display;
+
+    @JsonProperty("items")
+    private List<BookItem> items;
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class BookItem {
+
+        @JsonProperty("title")
+        private String title;
+
+        @JsonProperty("link")
+        private String link;
+
+        @JsonProperty("image")
+        private String image;
+
+        @JsonProperty("author")
+        private String author;
+
+        @JsonProperty("discount")
+        private String discount;
+
+        @JsonProperty("publisher")
+        private String publisher;
+
+        @JsonProperty("pubdate")
+        private String pubdate;
+
+        @JsonProperty("isbn")
+        private String isbn;
+
+        @JsonProperty("description")
+        private String description;
+    }
+}
